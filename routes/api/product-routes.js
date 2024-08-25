a96469ce7776b99a0af67c4b59d7a3cc79f03066
koliandrik <koliandrik@gmail.com>
2024-08-25 00:59:53 -0700

    forgot delete route on products

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -114,6 +114,13 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  Product.destroy({where: {id: req.params.id}})
+  .then((product) => {
+    res.json(product);
+  }).catch((err) => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 module.exports = router;
