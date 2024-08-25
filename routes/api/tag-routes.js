f30f03d5ef5d653bd4a3f7622c55d322f7f0fbe2
koliandrik <koliandrik@gmail.com>
2024-08-25 00:35:22 -0700

    updates rag routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -6,23 +6,64 @@ const { Tag, Product, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+  Tag.findAll({
+    include: [Product]
+  })
+  .then((tags) => {
+    res.json(tags);
+  }
+  )
 });
 
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  Tag.findOne({
+    where: {
+      tag_id: req.params.id
+    },
+    include: [Product]
+  })
+  .then((tag) => {
+    res.json(tag);
+  })
 });
 
 router.post('/', (req, res) => {
   // create a new tag
+  Tag.create(req.body)
+  .then((tag) => {
+    res.json(tag);
+  }
+  )
 });
 
 router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
+  Tag.update(req.body, {
+    where: {
+      tag_id: req.params.id
+    }
+  })
+  .then((tag) => {
+    res.json(tag);
+  }
+  )
 });
 
 router.delete('/:id', (req, res) => {
-  // delete on tag by its `id` value
+  // delete one tag by its `id` value
+  Tag.destroy({
+    where: {
+      tag_id: req.params.id
+    }
+  })
+  .then((tag) => {
+    res.json(tag);
+  })
+  .catch((err) => {
+    res.status(500).json(err);
+  });
 });
 
 module.exports = router;
